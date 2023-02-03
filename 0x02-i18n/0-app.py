bde3b9bb85cd4d3c6bfc52cce3c0d7c429e919af
amlaku <amlakubeyene@gmail.com>
2023-02-03 23:57:42 +0300

    new backend

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
-
-'''
-First you will setup a basic Flask app in `0-app.py`. Create a single `/` route
-and an `index.html` template that simply
-outputs "Welcome to Holberton" as page title (`<title>`)
-and "Hello world" as header (`<h1>`).
-'''
-
-from flask import Flask
-from flask import render_template
+"""A Basic Flask app.
+"""
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def home_page():
-    '''Returns homepage'''
+def get_index() -> str:
+    """The home/index page.
+    """
     return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
